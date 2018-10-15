7547dd47fe2be305298a370d24d00282da77b41f
August Pemberton <augustmpemberton@gmail.com>
2018-10-15 16:28:43 +0100

    Question 1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,10 +1,23 @@
 package answers;
 
+import java.util.Arrays;
+
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+
+		Arrays.parallelSort(portfolios);
+
+		int maxPortfolio = Integer.MIN_VALUE;
+		int val = 0;
+
+		for (int i=0;i<portfolios.length - 1; i++){
+			val = portfolios[i] ^ portfolios[i+1];
+			maxPortfolio = Math.max(maxPortfolio, val);
+		}
+
+
+		return maxPortfolio;
 	}
 
 }
